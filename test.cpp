6bd65186309afe4e8567f944c91ea8d7fb14146a
Jeanderson Candido <jeandersonbc@gmail.com>
2014-02-16 10:59:28 -0300

    Added example to be parsed

---
+++ test.cpp
@@ -0,0 +1,18 @@
+// This is a comment /* */ */
+
+/*  aisuhaisudh /
+ Aisuhdi // Asd
+ // Test *
+*/
+
+// Empty Stmt
+;
+
+// NamespaceDefinition
+namespace { }
+inline namespace { }
+namespace TestId { ; }
+inline namespace TestId {}
+namespace Nested {
+	namespace testId {}
+}
