2cfa70572584a827700f77dd19e85f0a3865c622
Jeanderson Candido <jeandersonbc@gmail.com>
2014-03-17 13:47:14 -0300

    Updated Main.java

--- src/compiler/main/Main.java
+++ src/compiler/main/Main.java
@@ -13,19 +13,40 @@ import compiler.generated.Parser;
  */
 public class Main {
 
-	public static void main(String[] args) {
+	/**
+	 * Minimum number of input
+	 */
+	private static final int MIN_INPUT_FILES = 1;
 
+	public static void main(String[] args) {
+		if (args.length < MIN_INPUT_FILES) {
+			displayHelpMessage();
+
+		} else {
+			for (String filePath : args) {
+				startCompilationFor(filePath);
+			}
+		}
+	}
+
+	private static void displayHelpMessage() {
+		System.out
+				.println("Usage: java -jar compiler.jar file [file2 file3...]");
+	}
+
+	private static void startCompilationFor(String filePath) {
 		try {
-			Lexer scanner = new Lexer(new BufferedReader(
-					new FileReader(args[0])));
+			System.out.println("Start compiling \"" + filePath + "\"...");
+			Lexer scanner = new Lexer(new BufferedReader(new FileReader(
+					filePath)));
 
 			Parser parser = new Parser(scanner);
 			parser.parse();
 
 		} catch (Exception e) {
+			System.err.println("Failed to compile \"" + filePath + "\":");
 			System.err.println(e.getMessage());
 		}
-
 	}
 
 }
